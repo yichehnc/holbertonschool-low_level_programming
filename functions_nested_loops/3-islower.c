4977294d805233b2c78cb8d3d0e1e8d70f2585a8
yichehnc <yichenc2017@gmail.com>
2023-10-20 16:20:32 -0700

    creates a function that checks lowercase characters

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase characters.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+        if (c >= 97 && c <= 122)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
