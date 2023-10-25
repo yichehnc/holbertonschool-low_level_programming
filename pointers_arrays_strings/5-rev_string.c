b815392b05d4498e7ac44ab4f59b6b30ca1c0b12
yichehnc <yichenc2017@gmail.com>
2023-10-25 06:17:29 -0700

    Write a function that reverses a string using two pointers

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - Sets two pointers at the start and end of the string.
+ *
+ * @s: the string reversed
+ *
+ * Return: s.
+ */
+void rev_string(char *s)
+{
+	char ch, *p, *q;
+
+	q = s;
+	while (*q != '\0')
+		q = q + 1;
+
+	while (q > s)
+	{
+		q = q - 1;
+	}
+
+	p = s;
+	while (p < q)
+	{
+		ch = *p;
+		*p = *q;
+		*q = ch;
+		p = p + 1;
+		q = q - 1;
+	}
+}
