dbda0426d29b03f00a06bc4a65b807654c7dc3a9
yichehnc <yichenc2017@gmail.com>
2023-11-16 12:52:43 -0800

    docs update header

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,23 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(inta, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
