361e4a60723781120cf4ba00543e93e4ceefbd56
yichehnc <yichenc2017@gmail.com>
2023-12-05 13:46:04 -0800

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_CREAT | O_WRONGLY | O_TURNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 	{
