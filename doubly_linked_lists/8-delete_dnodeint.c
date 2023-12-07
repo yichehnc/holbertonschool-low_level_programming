0789fe4ba76779a67bfefb070d37fb9536a8c0b1
yichehnc <yichenc2017@gmail.com>
2023-12-06 20:02:44 -0800

    debug checker

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -32,7 +32,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			nextN->next->prev = NULL;
 		}
 		free(nextN);
-		return (-1);
+		return (1);
 	}
 	while (i < index && nextN != NULL)
 	{
